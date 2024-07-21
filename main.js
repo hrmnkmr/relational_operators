bd3c5ea2f652bb018f888edc12a7af788abf5d79
nakamura <hrm.nkmr04@gmail.com>
2024-07-22 06:51:45 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var x = 3
+var y = 1
+if (x > y) {
+	console.log("xはyより大きい");
+}
+var x = 1
+var y = 3
+if (x < y) {
+	console.log("xはyより小さい");
+}
+var x = 3
+var y = 1
+if (x >= y) {
+	console.log("xはyより大きいまたは等しい");
+}
+var x = 1
+var y = 3
+if (x <= y) {
+	console.log("xはyより小さいまたは等しい");
+}
